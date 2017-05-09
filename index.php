79d16039798b9b06261110150cf3014a7fb9593f
karan kidarsharma <mearthkid@gmail.com>
2017-05-10 01:41:08 +1200

    jj

---
+++ index.php
@@ -0,0 +1,182 @@
+
+<!DOCTYPE html>
+<html oncontextmenu="return false">
+<head>
+<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+  <script src="bootstrap/js/jquery.min.js"></script>
+  <script src="bootstrap/js/bootstrap.min.js"></script>  
+<link rel="stylesheet" href="index.css">
+</head>
+<div style="background-color:#282e34">
+<nav class="navbar nav1">
+  <div class="container-fluid nav1">
+    <div class="navbar-header nav1" >
+      <a class="navbar-brand" style="color:white;" href="index.php">MegaWorld Tours! And Travels!</a>
+    </div>
+    <ul class="nav navbar-nav nav1">
+      <li class="active" ><a style="color:white;" href="#">Home</a></li>
+      <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" style="color:white;" href="#">Our Services
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#">Book Cabs</a></li>
+          <li><a href="#">Book Bus</a></li>
+          <li><a href="#">Book Hotel</a></li>
+        </ul>
+      </li>
+      <li><a style="color:white;" href="#">Offers!</a></li>
+      <li><a style="color:white;" href="#">Contact Us</a></li>
+    </ul>
+  </div>
+</nav>
+</div>
+<body>
+
+<div class="bgimg-1">
+  <div class="caption">
+    <span class="border">Welcome To Megaworld TourS And Travels!</span></br></br>
+    <span class="border">Travel The World! With Us!</span>
+  </div>
+</div>
+
+<div style="color: #777;background-color:white;text-align:center;padding:50px 80px;text-align: justify;">
+  <h3 style="text-align:center; font-size:50px;">Mega World Tour And Travels!</h3></br>
+  <h3 style="text-align:center;">We offer great service with great prices</h3>
+ <div class="container-fluid1 text-center">
+  <br>
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-globe logo"></span>
+      <h4>Booking</h4>
+      <p>We book online hotels..</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-heart logo"></span>
+      <h4>Tour Planing</h4>
+      <p>We Provide great packages to the people who love to travel..</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-plane logo"></span>
+      <h4>Flight Tickets</h4>
+      <p>We book flight tickets..</p>
+    </div>
+  </div>
+  <br><br>
+  <div class="row">
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-bed logo"></span>
+      <h4>Train Ticket</h4>
+      <p>We book train tickets..</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-certificate logo "></span>
+      <h4>CERTIFIED</h4>
+      <p>We are in the market since 2003..</p>
+    </div>
+    <div class="col-sm-4">
+      <span class="glyphicon glyphicon-phone-alt logo"></span>
+      <h4 style="color:#303030;">Online Support</h4>
+      <p>We are ready to help you..</p>
+    </div>
+  </div>
+</div>
+
+</div>
+
+<div class="bgimg-2">
+  <div class="caption">
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">TRAVELLING LOVERS</span>
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+    
+<p>You chat about your respective evenings. He puts on a Blur album; you suggest To Pimp a Butterfly instead. By the time Kendrick sings, “But your flood can be misunderstood,” he’ll have casually slid next to you on the air mattress.
+Will it be a fun night? Oh, yes.
+But there’s a casualty — it will make things weird with your friend. Your friend who was so kind to offer you a place to stay, to take you out for dinner and drinks, to catch up on everything that’s been happening in the years since you drank buckets together in ramshackle bars built by the water, cockroaches scurrying by your feet.
+You’re over 30 now. And you still pull that shit?
+</p>
+<p>He doesn’t eat seafood, but you do. Served up is a giant plate of shrimp, dripping in fresh lime juice and punctuated by zests of garlic and chili. This isn’t pad grapow on Soi Rambuttri — this is real Thai food for Thai people. And perhaps that’s why you’re caught off guard.</p>
+<p>You’re fine as you catch your early morning flight to Ranong. You’re fine as you cross the choppy waters to Koh Phayam, gazing at Myanmar in the distance. You’re fine on the kamikaze motorbike ride to your guesthouse as you drink in the warm breezes of the Andaman Sea.
+36 hours after your fateful meal, it hits you. Stomach cramps. Nausea. Fever. Diarrhea. Running to the bathroom four times an hour and feeling like you’re going to die each time.
+Years into your long-term travels, this is only the second time you’ve fallen victim to food poisoning; you’re proud of your cast-iron stomach. But everyone has their kryptonite.
+The diarrhea isn’t even the worst part. Soon, the stomach cramps get so bad that you can’t sit up — you can only rock back and forth on the bed, moaning in place. Staying still is impossible. Sitting up is impossible. All you can do is whimper and turn down offers of plain sticky rice from the concerned ladies checking on you.</p>
+
+
+  </div>
+</div>
+
+<div class="bgimg-3">
+  <div class="caption">
+    <span class="border" style="background-color:transparent;font-size:25px;color: #f7f7f7;">PLANNING A TRIP LET US KNOW !</span>
+  </div>
+</div>
+
+<div style="position:relative;">
+  <div style="color:#ddd;background-color:#282E34;text-align:center;padding:50px 80px;text-align: justify;">
+   <h3 style="color:#ddd"> Why Megaworld Tour And Travels?</h3>
+
+<p>The leading player in online flight bookings in India, Megaworld Tour And Travels offers great offers, some of the lowest airfares, exclusive discounts and a seamless online booking experience. Flight, hotel and holiday bookings through the desktop or mobile site is a delightfully customer friendly experience, and with just a few clicks you can complete your booking. With features like Instant Discounts, Fare Calendar, MyRewards Program, MyWallet and many more, the overall booking experience with Megaworld Tour And Travels constantly adds value to its product and continues to offer the best to its customers.</p></br>
+
+<h3 style="color:#ddd">Booking Flights with Megaworld Tour And Travels</h3>
+
+<p>Book your flights tickets with India’s leading flight booking company since the year 2000. While booking flights with Megaworld Tour And Travels, you can expect the ultimate online booking experience. With premium customer service, 24/7 dedicated helpline for support, and over 5 million delighted customers, Megaworld Tour And Travels takes great pride in enabling customer satisfaction. With a cheapest flight guarantee, book your tickets at the lowest airfares. Avail great offers, exclusive deals for loyal customers and get instant updates for your flight status and fare drops.</p>
+</br>
+<h3 style="color:#ddd">Domestic Flights with Megaworld Tour And Travels</h3>
+
+<p>Megaworld Tour And Travels is India’s leading player for flight bookings, and have a dominant position in the domestic flights sector. With the cheapest fare guarantee, experience great value at the lowest price. Instant notifications ensure current flight status, instant fare drops, amazing discounts, instant refunds and rebook options, price comparisons and many more interesting features.</p>
+  </div>
+</div>
+
+<div class="bgimg-1">
+  <div class="caption">
+    <span class="border">LIFE IS SO BEAUTIFUL!</span>
+  </div>
+</div>
+
+</body>
+<footer>
+  <div class="jumbotron text-center">
+  <h1>MegaWorld Tours And Travels!</h1> 
+ <strong> <p style="font-size:15px; color:white; font-family:Century Gothic; text-align:left;">The leading player in online flight bookings in India, Megaworld Tour And Travels offers great offers, some of the lowest airfares, exclusive discounts and a seamless online booking experience. Flight, hotel and holiday bookings through the desktop or mobile site is a delightfully customer friendly experience, and with just a few clicks you can complete your booking. With features like Instant Discounts, Fare Calendar, MyRewards Program, MyWallet and many more, the overall booking experience with Megaworld Tour And Travels constantly adds value to its product and continues to offer the best to its customers.</p> </strong>
+  
+
+
+<div class="container-fluid " style="width:80%;">
+  <h2 class="text-center">CONTACT</h2>
+  <div class="row">
+    <div class="col-sm-5">
+      <p>Contact us and we'll get back to you within 24 hours.</p>
+      <p><span class="glyphicon glyphicon-map-marker"></span> J&k, INDIA</p>
+      <p><span class="glyphicon glyphicon-phone"></span> +91 9697570000</p>
+      <p><span class="glyphicon glyphicon-envelope"></span> contact@megaworld.co.in</p>
+    </div>
+  </br>
+    <div class="col-sm-7">
+      <div class="row">
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="name" name="name" placeholder="Name" type="text" required>
+        </div>
+        <div class="col-sm-6 form-group">
+          <input class="form-control" id="email" name="email" placeholder="Email" type="email" required>
+        </div>
+      </div>
+      <textarea class="form-control" id="comments" name="comments" placeholder="Comment" rows="5"></textarea><br>
+      <div class="row">
+        <div class="col-sm-12 form-group">
+          <button class="btn btn-info pull-right" type="submit">Send</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+</div>
+  </footer>
+</html>
